7c00df19c6a77c83edcbcb6becb1e6d1c9031f06
Ivna <ivnafeitosa113@gmail.com>
2022-07-21 20:38:21 -0300

    first commit

---
+++ src/paraogit/primeiroprojeto/PrimeiroProjeto.java
@@ -0,0 +1,13 @@
+package paraogit.primeiroprojeto;
+
+import paraogit.primeiroprojeto.model.Gato;
+
+public class PrimeiroProjeto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Gato gato=new Gato();
+		System.out.println(gato);
+	}
+
+}
